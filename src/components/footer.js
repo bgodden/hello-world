559deb31c773416e8959b53604d5769cca253b4a
Brian Godden <bpg@me.com>
2020-11-07 07:48:10 -0800

    New website

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default function Footer () {
+    return (
+        <footer>
+                <Link to="/">Name of our site</Link>
+                <Link to="/portfolio">Portfolio</Link>
+        </footer>
+    )
+}
